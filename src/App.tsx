78a2a29b47424c17c8327243e483261a1c30cfe7
Adham Hassan <49523561+adhamhassan99@users.noreply.github.com>
2023-04-04 15:43:34 +0200

    added fixed socials and clipboard copyy

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,50 @@ import { AboutMe, Hero, NavBar, Section } from "./components";
 import WorkedPlacesSelector from "./components/WorkedPlacesSelector/WorkedPlacesSelector";
 import ContactMe from "./components/ContactMe/ContactMe";
 import Credits from "./components/Credits/Credits";
+import FixedSocials from "./components/FixedSocials/FixedSocials";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const handleScroll = (event: any) => {
     console.log(event);
   };
   return (
-    <div
-      onScrollCapture={(event) => console.log(event)}
-      className="App bg-Navy w-full "
-    >
-      <NavBar />
-      <Section id="hero" className="">
-        <Hero />
-      </Section>
-      <Section className="" id="about">
-        <AboutMe />
-      </Section>
-      <Section className="" id="experience">
-        <WorkedPlacesSelector />
-      </Section>
-      <Section id="work">c</Section>
-      <Section className="pt-0 pb-0 mb-0 h-[80vh]" id="contact">
-        <ContactMe />
-      </Section>
-      <Credits />
-    </div>
+    <>
+      <div
+        onScrollCapture={(event) => console.log(event)}
+        className="App bg-Navy w-full "
+      >
+        <FixedSocials />
+        <NavBar />
+        <Section id="hero" className="">
+          <Hero />
+        </Section>
+        <Section className="" id="about">
+          <AboutMe />
+        </Section>
+        <Section className="" id="experience">
+          <WorkedPlacesSelector />
+        </Section>
+        <Section id="work">c</Section>
+        <Section className="pt-0 pb-0 mb-0 h-[80vh] md:h-[90vh]" id="contact">
+          <ContactMe />
+        </Section>
+        <Credits />
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </div>
+    </>
   );
 }
 
