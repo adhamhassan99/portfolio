4780a5909c1113f62fe9633f842691e5dc777836
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-04-21 21:35:21 +0200

    finish projects

--- src/components/ProjectItem/ProjectItems.tsx
+++ src/components/ProjectItem/ProjectItems.tsx
@@ -44,7 +44,7 @@ const ProjectItems = (props: Props) => {
           </div>
         );
       })} */}
-      <div className="mt-0 grid w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="mt-0 grid w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.sort().map((item, index) => (
           <GridItem {...item} />
         ))}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,60 +2,48 @@ import dash from "../assets/dash.png";
 export const projects = [
   {
     projectName: "Automotive Dashboard",
-    projectDescription: "lorem ipsum dolor sit amet, consectetur adipiscing ",
+    projectDescription:
+      "A Dashboard for renting cars and viewing them and their statistics. Implemented as a first React project",
     techUsed: [
       "React",
-      "SCSS",
+      "TypeScript",
+      "SASS",
       "Firebase Authentication",
       "Firebase Firestore",
       "Redux",
     ],
-    projectLink: "url",
+    projectLink: "https://autodashboard.vercel.app/auth/signin",
+    githubLink:
+      "https://github.com/adhamhassan99/AutomotiveDashboard/tree/main/auto-dash",
 
     previewImg: dash,
   },
   {
-    projectName: "Automotive Dashboard",
-    projectDescription: "lorem ipsum dolor sit amet, consectetur adipiscing",
+    projectName: "Weather App",
+    projectDescription:
+      "A Weather app that uses the open weather service to provide weather information based on desired locaation",
     techUsed: [
       "React",
-      "SCSS",
-      "Firebase Authentication",
-      "Firebase Firestore",
+      "TypeScript",
+      "TailwindCSS",
+      "TanStack React Query",
+      "Axios",
+      "MaterialUI",
       "Redux",
     ],
-    projectLink: "url",
-    githubLink: "github",
+    projectLink: "https://weather-app-sand-kappa.vercel.app/",
+    githubLink:
+      "https://github.com/adhamhassan99/WeatherApp/tree/main/weather-app",
     previewImg: dash,
   },
 
   {
-    projectName: "Automotive Dashboard",
-    projectDescription: "lorem ipsum dolor sit amet, consectetur adipiscing",
-    techUsed: [
-      "React",
-      "SCSS",
-      "Firebase Authentication",
-      "Firebase Firestore",
-      "Redux",
-    ],
-    projectLink: "url",
-    githubLink: "github",
-    previewImg: dash,
-  },
-
-  {
-    projectName: "Automotive Dashboard",
-    projectDescription: "lorem ipsum dolor sit amet, consectetur adipiscing",
-    techUsed: [
-      "React",
-      "SCSS",
-      "Firebase Authentication",
-      "Firebase Firestore",
-      "Redux",
-    ],
-    projectLink: "url",
-    githubLink: "github",
+    projectName: "E-commerce App",
+    projectDescription:
+      "E-commerce App with firebase authentication and cart functionality implemented",
+    techUsed: ["React", "Javascript", "SASS", "Redux"],
+    projectLink: "https://ztm-e-commerce-crwn.vercel.app/",
+    githubLink: "https://github.com/adhamhassan99/ZTM_E-commerce_CRWN",
     previewImg: dash,
   },
 ];
